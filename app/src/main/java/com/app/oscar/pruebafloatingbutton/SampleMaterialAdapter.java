6a04ac763cee425e2f8e92853b56f4fd8ec05b3c
Bol8 <oscarbolon@hotmail.com>
2016-10-15 13:45:36 +0200

    Ajustes.

--- app/src/main/java/com/app/oscar/pruebafloatingbutton/SampleMaterialAdapter.java
+++ app/src/main/java/com/app/oscar/pruebafloatingbutton/SampleMaterialAdapter.java
@@ -10,6 +10,7 @@ import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
@@ -29,7 +30,43 @@ public class SampleMaterialAdapter extends RecyclerView.Adapter<SampleMaterialAd
     public ArrayList<Card> cardsList;
 
 
+    public SampleMaterialAdapter(Context context, ArrayList<Card> cardsList) {
+        this.context = context;
+        this.cardsList = cardsList;
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        String name = cardsList.get(position).getName();
+        int color = cardsList.get(position).getColorResource();
+        TextView initial = viewHolder.initial;
+        TextView nameTextView = viewHolder.name;
+        nameTextView.setText(name);
+        initial.setBackgroundColor(color);
+        initial.setText(Character.toString(name.charAt(0)));
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(ViewHolder viewHolder) {
+        super.onViewDetachedFromWindow(viewHolder);
+        viewHolder.itemView.clearAnimation();
+    }
+
+    @Override
+    public void onViewAttachedToWindow(ViewHolder viewHolder) {
+        super.onViewAttachedToWindow(viewHolder);
+        animateCircularReveal(viewHolder.itemView);
+    }
+
+    public void animateCircularReveal(View view) {
+        int centerX = 0;
+        int centerY = 0;
+        int startRadius = 0;
+        int endRadius = Math.max(view.getWidth(), view.getHeight());
+        Animator animation = ViewAnimationUtils.createCircularReveal(view, centerX, centerY, startRadius, endRadius);
+        view.setVisibility(View.VISIBLE);
+        animation.start();
+    }
 
     public void animateCircularDelete(final View view, final int list_position) {
         int centerX = view.getWidth();
@@ -54,22 +91,46 @@ public class SampleMaterialAdapter extends RecyclerView.Adapter<SampleMaterialAd
         animation.start();
     }
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public void addCard(String name, int color) {
+        Card card = new Card();
+        card.setName(name);
+        card.setColorResource(color);
+        card.setId(getItemCount());
+        cardsList.add(card);
+        ((MainActivity) context).doSmoothScroll(getItemCount());
+        notifyItemInserted(getItemCount());
     }
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void updateCard(String name, int list_position) {
+        cardsList.get(list_position).setName(name);
+        Log.d(DEBUG_TAG, "list_position is " + list_position);
+        notifyItemChanged(list_position);
+    }
 
+    public void deleteCard(View view, int list_position) {
+        animateCircularDelete(view, list_position);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (cardsList.isEmpty()) {
+            return 0;
+        } else {
+            return cardsList.size();
+        }
     }
 
+    @Override
+    public long getItemId(int position) {
+        return cardsList.get(position).getId();
+    }
 
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        LayoutInflater li = LayoutInflater.from(viewGroup.getContext());
+        View v = li.inflate(R.layout.card_view_holder, viewGroup, false);
+        return new ViewHolder(v);
+    }
 
     class ViewHolder extends RecyclerView.ViewHolder {
         private TextView initial;
